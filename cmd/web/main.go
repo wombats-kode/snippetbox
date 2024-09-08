33f70360e344e70952763c3d02ac38acafa09d88
wombats-kode <wombat1913@gmail.com>
2024-09-08 14:32:26 +0100

    added structured logging into main.go

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -2,8 +2,9 @@ package main
 
 import (
 	"flag"
-	"log"
+	"log/slog"
 	"net/http"
+	"os"
 )
 
 func main() {
@@ -12,6 +13,10 @@ func main() {
 	// will be stored in the addr variable at runtime.
 	addr := flag.String("addr", ":4000", "HTTP network address")
 	flag.Parse()
+
+	// Use the slog.New() function to initialise a new structured logger, which
+	// writes to the standard out stream and used the default settings.
+	logger := slog.New(slog.NewTextHandler(os.Stdout, nil))
 
 	mux := http.NewServeMux()
 
@@ -33,8 +38,17 @@ func main() {
 
 	// The value returned from the flag.String() function is a pointer to a the flag
 	// value itself, so needs to be de-referenced before using it.
-	log.Printf("Starting server on %s", *addr)
+
+	// Use the Info() method to log the server start message
+	logger.Info("starting server", "addr", *addr)
 
 	err := http.ListenAndServe(*addr, mux)
-	log.Fatal(err)
+
+	// And we can use the Error() method to log any error message returned by
+	// ListenAndServe() at Error severity and then call os.Exit(1) to exit with
+	// exit code (1).
+	logger.Error(err.Error())
+	os.Exit(1)
+	// Note: There is not slog equivalent to the log.Fatal() function so we have to
+	// write the Error and then close the application with os.Exit().
 }
