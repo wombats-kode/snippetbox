ba6effb21f879f7f0976d362cd92ec4b322fb8ed
wombats-kode <wombat1913@gmail.com>
2024-09-20 19:02:38 +0100

    add  middleware packages

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -3,7 +3,9 @@ package main
 import "net/http"
 
 // The routes() method returns a servemux containing our application routes.
-func (app *application) routes() *http.ServeMux {
+// Update the signature for the routes() method so that is returns a http.Handler
+// instead of *http.ServeMux to support our middleware patterns.
+func (app *application) routes() http.Handler {
 	mux := http.NewServeMux()
 
 	fileserver := http.FileServer(http.Dir("./ui/static/"))
@@ -14,5 +16,8 @@ func (app *application) routes() *http.ServeMux {
 	mux.HandleFunc("GET /snippet/create", app.snippetCreate)
 	mux.HandleFunc("POST /snippet/create", app.snippetCreatePost)
 
-	return mux
+	// Pass the serveMux as the 'next' parameter to the commonHeaders middleware
+	// Because commonHeader is jut a function and returns a http.Hander we dont
+	// need to anything else.
+	return app.recoverPanic(app.logRequest(commonHeaders(mux)))
 }
--- cmd/web/templates.go
+++ cmd/web/templates.go
@@ -3,6 +3,7 @@ package main
 import (
 	"html/template"
 	"path/filepath"
+	"time"
 
 	"snippetbox.example.com/internal/models"
 )
@@ -15,6 +16,22 @@ type templateData struct {
 	Snippets    []models.Snippet
 }
 
+// Function that returns a nicely formatted string representation of
+// a time.Time object.
+func humanDate(t time.Time) string {
+	return t.Format("02 Jan 2006 at 15:04")
+}
+
+// Initialise a template.FuncMap object and store it in a global variable.
+// This is essentially a string-keyed map which acts as a lookup between the
+// names of our custom template functions and the functions themselves.
+// Parse the base template page into a template set.
+var functions = template.FuncMap{
+	"humanDate": humanDate,
+}
+
+// Function that returns a cache containing html templates and a customer
+// FuncMap for use in generating response output.
 func NewTemplateCache() (map[string]*template.Template, error) {
 	// Initialise a new map to act as the cache.
 	cache := map[string]*template.Template{}
@@ -34,13 +51,16 @@ func NewTemplateCache() (map[string]*template.Template, error) {
 		// and assign it to the name variable.
 		name := filepath.Base(page)
 
-		// Parse the base template page into a template set.
-		ts, err := template.ParseFiles("./ui/html/base.tmpl")
+		// The template.FuncMap must be registered with the template set before you
+		// call the ParseFiles() method. This means we have to use template.New() to
+		// create an empty template set, use the Funcs() method to register the
+		// template.FuncMap, and then parse the file as normal.
+		ts, err := template.New(name).Funcs(functions).ParseFiles("./ui/html/base.tmpl")
 		if err != nil {
 			return nil, err
 		}
 
-		// Call ParseGlob() on *on this template set* to add any partials.
+		// Call ParseGlob() *on this template set* to add any partials.
 		ts, err = ts.ParseGlob("./ui/html/partials/*.tmpl")
 		if err != nil {
 			return nil, err
